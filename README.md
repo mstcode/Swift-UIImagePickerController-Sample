bddc68ce066fa6c63e79c1ed60c61b5307a7cfdb
mstcode <info@mstcode.com>
2015-12-15 15:17:28 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+##Swift UIImagePickerController Tutorial
+
+Source code on adding UIImagePickerController in Swift 2 Using Xcode 7.2
+
+Tutorial: https://www.youtube.com/watch?v=YYS-LwvluL8
+
+Check out our Website: http://www.mstcode.com
+
+Follow us on Twtitter: https://twitter.com/mstcode_
+
+Like us on Facebook: http://on.fb.me/1OcsVuX
---
+++ UIImagePickerController/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  UIImagePickerController
+//
+//  Created by mstcode on 15/12/15.
+//  Copyright © 2015 mstcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func takePhoto(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func selectPhoto(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
